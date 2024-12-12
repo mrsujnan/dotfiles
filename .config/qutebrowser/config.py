804a527004eebca28c2e16f19e1866627c69551d
mrsujnan <sujnan@duck.com>
2024-12-12 16:42:34 +0530

    qutebrowser dark mode

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -3,9 +3,10 @@ config.source("themes/qute-city-lights/city-lights-theme.py")
 # Enable dark mode for the browser UI
 # c.qt.args = ["--force-dark-mode"]
 
-# config.set("colors.webpage.darkmode.enabled", True)
+config.set("colors.webpage.darkmode.enabled", True)
+
 # config.set("content.user_stylesheets", ["~/.config/qutebrowser/darkmode.css"])
 
-# config.bind("<Ctrl-d>", "config-cycle colors.webpage.darkmode.enabled")
+config.bind("<Ctrl-d>", "config-cycle colors.webpage.darkmode.enabled")
 
 config.load_autoconfig()
